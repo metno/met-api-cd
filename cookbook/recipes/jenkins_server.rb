0c4e733205e6e07c47a26ad7687f2373460169cd
Kim Tore Jensen <kimtj@met.no>
2014-10-30 14:02:26 +0100

    Cookbook: install nginx on Jenkins master, proxying to Jenkins servlet.

--- cookbook/recipes/jenkins_server.rb
+++ cookbook/recipes/jenkins_server.rb
@@ -7,6 +7,34 @@
 
 include_recipe 'jenkins::master'
 
+include_recipe 'nginx'
+
+template '/etc/nginx/sites-available/jenkins-server.conf' do
+    source 'nginx/jenkins.conf.erb'
+    owner 'root'
+    group 'root'
+    mode '0640'
+end
+
+link '/etc/nginx/sites-enabled/jenkins-server.conf' do
+    to '/etc/nginx/sites-available/jenkins-server.conf'
+end
+
+firewall_rule "http" do
+    port 80
+    source '0.0.0.0/0'
+    protocol :tcp
+    action :allow
+end
+
+if node[:'met-bora-ci'][:nginx][:ssl]
+    firewall_rule "https" do
+        port 443
+        source '0.0.0.0/0'
+        protocol :tcp
+        action :allow
+    end
+end
 include_recipe 'met-bora-ci::_common'
 
 jenkins_plugin 'git'
--- cookbook/test/integration/jenkins-server/bats/jenkins.bats
+++ cookbook/test/integration/jenkins-server/bats/jenkins.bats
@@ -7,6 +7,20 @@
 }
 
 @test "Jenkins is running on port 8080" {
-    run curl -i http://localhost:8080
+    run curl -f http://localhost:8080
     [ $status -eq 0 ]
+}
+
+@test "nginx is installed" {
+    run which nginx
+    [ $status -eq 0 ]
+}
+
+@test "nginx is proxying to Jenkins" {
+    run curl -f http://localhost
+    [ $status -eq 0 ]
+    curl -f http://localhost | {
+        run grep Jenkins
+        [ $status -eq 0 ]
+    }
 }
